9408313c8353f1a21d69c37e8e0c450cda8a6d4d
shuvo-me <shuvo.js.dev@gmail.com>
2025-05-21 11:37:17 +0600

    core: uploading project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { Editor, EditorContent, useEditor } from '@tiptap/vue-3'
+import StarterKit from '@tiptap/starter-kit';
+import {ref, onMounted, onBeforeUnmount} from 'vue'
+
+ const editor = useEditor({
+    content: "<p>I'm running Tiptap with Vue.js. 🎉</p>",
+    extensions: [StarterKit],
+    injectCSS: false,
+    editorProps: {
+      attributes: {
+        class: 'editor-content',
+      },
+    },
+  });
+</script>
+
+<template>
+  <div class="editor-area">
+     <editor-content :editor="editor" class="editor-content" />
+  </div>
+</template>
