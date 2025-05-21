77cf2b23f8e91802c7e813e12a710dc4eacd3a54
shuvo-me <shuvo.js.dev@gmail.com>
2025-05-21 11:44:01 +0600

    fix: removed unused imports

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import { Editor, EditorContent, useEditor } from '@tiptap/vue-3'
+import { EditorContent, useEditor } from '@tiptap/vue-3'
 import StarterKit from '@tiptap/starter-kit';
-import {ref, onMounted, onBeforeUnmount} from 'vue'
 
  const editor = useEditor({
     content: "<p>I'm running Tiptap with Vue.js. 🎉</p>",
